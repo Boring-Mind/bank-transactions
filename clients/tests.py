a4f3e7b51ae2490e62217e1a11db998a52d1e034
Boring-Mind <super.d3280@ya.ru>
2020-08-01 03:16:18 -0700

    test: added test for password hashing for Client model

--- clients/tests.py
+++ clients/tests.py
@@ -1,4 +1,27 @@
+from django.contrib.auth.hashers import make_password
 from django.test import TestCase
 from rest_framework.test import APITestCase
 
+from .models import Client
 
+
+class ModelIntegrationTests(APITestCase):
+	def test_password_is_saved_as_hash(self):
+		"""Ensure password is stored as correct hash.
+
+		For hashing we use default hasher
+		which is listed in Django settings.
+		"""
+		password = 'SomeP@ssw0rd'
+		client = Client.objects.create(
+			username='somename',
+			password=password,
+			phone_number='012345678912345',
+			passport_number='0123456789'
+		)
+
+		self.assertEquals(Client.objects.count(), 1)
+
+		expected = make_password(password)
+
+		self.assertEquals(client.password, expected)
