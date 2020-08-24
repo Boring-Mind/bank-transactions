710f30339a10ec12e0f0284684cce8de79a1915e
Boring-Mind <super.d3280@ya.ru>
2020-08-24 02:04:34 -0700

    tests: added and configured pytest-cov. Written tests for sha512 generation.

--- utils/tests.py
+++ utils/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+import re
 
-# Create your tests here.
+from .hashers import generate_unique_sha_512
+
+
+def test_generate_unique_sha_512_returns_random_hashes():
+    """Check that function returns two random sha-512 hashes."""
+    hash1 = generate_unique_sha_512()
+    hash2 = generate_unique_sha_512()
+
+    assert re.fullmatch(r'[\da-f]{64}', hash1)
+    assert re.fullmatch(r'[\da-f]{64}', hash2)
+
+    assert hash1 != hash2
