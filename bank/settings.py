e537114c0dd1402c9beb579d78c31519cb6da64b
Boring-Mind <super.d3280@ya.ru>
2020-09-29 09:29:22 -0700

    feet: switched to the PostgreSQL db backend.

--- bank/settings.py
+++ bank/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 from datetime import timedelta
 
+import dj_database_url
 from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -88,10 +89,7 @@ WSGI_APPLICATION = 'bank.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse(config('PSQL_URL')),
 }
 
 LOGGING = {
